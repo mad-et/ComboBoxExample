5d7ef9f24915c32ac7684c2e8f47cff31138bea8
Екатерина Швец <mad-et@outlook.com>
2023-09-01 18:41:49 +0300

    Добавлены комментарии в коде

--- ComboBoxExample/ViewModel/MainWindowViewModel.cs
+++ ComboBoxExample/ViewModel/MainWindowViewModel.cs
@@ -18,9 +18,19 @@ namespace ComboBoxExample.ViewModel
             Elements = new List<Element>();
         }
 
-        #region Categories       
+        
+        #region Categories  
+        /// <summary>
+        /// Свойство, привязанное к верхнему комбобоксу
+        /// </summary>
         public List<Category> Categories { get; set; }
-       
+
+        /// <summary>
+        /// Выбранный элемент в верхнем комбобоксе. 
+        /// Это свойство "развернули", т.е. используем не сокращенную форму типа public Category SelectedCategory { get; set; } специально, 
+        /// чтобы в части set (в момент выбора значения из верхнего комбобокса,
+        /// вызвать поиск элементов из категории и занести их во второй комбобокс
+        /// </summary>
         private Category selectedCategory;
         public Category SelectedCategory
         {
@@ -33,8 +43,15 @@ namespace ComboBoxExample.ViewModel
         }
         #endregion
 
-        #region Types       
-        public List<Element> Elements { get; set; }        
+        #region Elements   
+        /// <summary>
+        /// Свойство, привязанное к нижнему комбобоксу
+        /// </summary>
+        public List<Element> Elements { get; set; }
+
+        /// <summary>
+        /// Выбранный элемент в нижнем комбобоксе.
+        /// </summary>
         public Element SelectedElement { get; set; }
         #endregion
     }
